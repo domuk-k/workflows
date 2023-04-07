c11dc3d5a1880c4f26b86b435d3e991bb89746f6
domuk-k <dannyworks102@gmail.com>
2023-04-07 16:55:53 +0900

    fix: npx test

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,4 @@
 const { execSync } = require('child_process');
 
-console.log(execSync('cowsay asdf').toString());
+console.log(execSync('npx cowsay asdf').toString());
+console.log(execSync('npx datadog-ci version').toString());
