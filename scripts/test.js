b29775ec56f43c8da28fdb2b47d82af320030e4a
domuk-k <dannyworks102@gmail.com>
2023-04-10 17:19:53 +0900

    fix: test

--- scripts/test.js
+++ scripts/test.js
@@ -2,3 +2,11 @@ const { execSync } = require('child_process');
 
 console.log(execSync('npx cowsay asdf').toString());
 console.log(execSync('npx @datadog/datadog-ci version').toString());
+
+const fs = require('fs');
+
+if (fs.existsSync('/wowow')) {
+  console.log('noo');
+} else {
+  console.log('pass');
+}
