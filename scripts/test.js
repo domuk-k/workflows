cc11a357302da8042350f8ee5c2c613984acc765
domuk-k <dannyworks102@gmail.com>
2023-04-10 18:31:08 +0900

    fix: SERVICE_VERSION

--- scripts/test.js
+++ scripts/test.js
@@ -10,6 +10,11 @@ console.log(process.env.JOB_LEVEL_SERVICE_VERSION);
 console.log(process.env.STEP_LEVEL_SERVICE_VERSION);
 console.log(process.env.MY_VAR);
 console.log(process.env.WORKFLOW_LEVEL_SERVICE_VERSION_REF_NAME);
+console.log('SERVICE_VERSION', process.env.SERVICE_VERSION);
+console.log(
+  'WORKFLOW_LEVEL_SERVICE_VERSION_REF_NAME',
+  process.env.WORKFLOW_LEVEL_SERVICE_VERSION_REF_NAME
+);
 console.log(process.env.action_state);
 if (fs.existsSync('/wowow')) {
   console.log('noo');
